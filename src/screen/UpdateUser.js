2da34104be5da784faa98547a696dc49efff8e7b
Irsannerha <irsanrh275@gmail.com>
2023-05-15 17:45:08 +0700

    Upload Code Final

--- src/screen/UpdateUser.js
+++ src/screen/UpdateUser.js
@@ -77,3 +77,61 @@ const UpdateUser = () => {
         </View>
       );
     };
+
+    const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          backgroundColor: '#f0f8ff',
+          borderRadius: 10,
+          padding: 50,
+          paddingTop: 70,
+        },
+        centered: {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        userImage: {
+          width: '100%',
+          height: 200,
+          marginBottom: 20,
+        },
+        uploadIcon: {
+          width: 50,
+          height: 50,
+          marginRight: 10,
+        },
+        imagePickerButton: {
+          backgroundColor: '#fff',
+          padding: 10,
+          borderRadius: 5,
+          alignItems: 'center',
+          marginBottom: 20,
+        },
+        imagePickerButtonText: {
+          fontSize: 18,
+        },
+        textInput: {
+          borderWidth: 1,
+          borderColor: '#F50',
+          padding: 10,
+          borderRadius: 5,
+          marginBottom: 20,
+          width: '100%',
+          color: '#000',
+        },
+        changePasswordButton: {
+          backgroundColor: '#DF0606',
+          padding: 10,
+          borderRadius: 5,
+          alignItems: 'center',
+        },
+        changePasswordButtonText: {
+          color: '#fff',
+          fontSize: 18,
+          fontWeight: 'bold'
+        },
+      });
+      
+      export default UpdateUser;
+      
