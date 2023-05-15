1733a8ea1f2ebf8cf2912e23cbdd7a59b62abf11
Irsannerha <irsanrh275@gmail.com>
2023-05-15 17:43:32 +0700

    Upload Code Final

--- src/screen/UpdateUser.js
+++ src/screen/UpdateUser.js
@@ -10,3 +10,21 @@ import {
 import ImagePicker from 'react-native-image-picker';
 import BackButton from '../components/BackButton';
 import { useNavigation } from '@react-navigation/native';
+
+const UpdateUser = () => {
+    const [userImage, setUserImage] = useState(null);
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const navigation = useNavigation();
+  
+    const handleImagePicker = () => {
+      ImagePicker.showImagePicker({ title: 'Select Image' }, (response) => {
+        if (response.uri) {
+          setUserImage(response.uri);
+        }
+      });
+    };
+  
+    const handleChangePassword = () => {
+      // Perform actions for changing password here
+    };
