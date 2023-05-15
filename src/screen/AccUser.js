d49443465b94830b92ebc4a3df77f367dc188c32
Irsannerha <irsanrh275@gmail.com>
2023-05-15 18:01:31 +0700

    Upload Code Final

--- src/screen/AccUser.js
+++ src/screen/AccUser.js
@@ -4,3 +4,20 @@ import BackButton from '../components/BackButton';
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../context/auth-context';
 import Toast from 'react-native-toast-message';
+
+const AccUser = () => {
+  const navigation = useNavigation();
+  const { user, updateToken, updateUser } = useAuth();
+
+  const handleLogout = () => {
+    updateToken(null);
+    updateUser({});
+    navigation.navigate('LoginScreen');
+    Toast.show({
+      type: 'success',
+      text1: 'Success',
+      text2: 'You have successfully logged out!',
+      visibilityTime: 5000,
+      autoHide: true,
+    });
+  };
