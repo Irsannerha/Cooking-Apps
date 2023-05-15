3677b46da4d4fc7aa7cdd267e90d7917956b6c6c
anggrndewi <dewi.120140149@student.itera.ac.id>
2023-05-15 07:48:34 +0700

    Membuat input untuk Ganti Password

--- src/screen/UpdatePass.js
+++ src/screen/UpdatePass.js
@@ -8,5 +8,29 @@ const UpdatePass = () => {
     const handleSavePassword = () => {
       // Save new password to database or perform other actions here
     };
-   
+
+    return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Ganti Password</Text>
+      <TextInput
+        secureTextEntry
+        placeholder="Masukkan Password Lama"
+        style={styles.textInput}
+        value={oldPassword}
+        onChangeText={text => setOldPassword(text)}
+      />
+      <TextInput
+        secureTextEntry
+        placeholder="Masukkan Password Baru"
+        style={styles.textInput}
+        value={newPassword}
+        onChangeText={text => setNewPassword(text)}
+      />
+      <TouchableOpacity style={styles.saveButton} onPress={handleSavePassword}>
+        <Text style={styles.saveButtonText}>Simpan</Text>
+      </TouchableOpacity>
+    </View>
+      );
 };
+
+export default UpdatePass;
