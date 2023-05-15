d63f1ea16677f023f2a406faf300fed816c26ac1
anggrndewi <dewi.120140149@student.itera.ac.id>
2023-05-15 08:14:21 +0700

    Create const UpsateUser and handleImagePicker

--- src/screen/UpdateUser.js
+++ src/screen/UpdateUser.js
@@ -1,3 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet } from "react-native";
 import ImagePicker from 'react-native-image-picker';
+
+const UpdateUser = () => {
+    const [userImage, setUserImage] = useState(null);
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+
+    const handleImagePicker = () => {
+        ImagePicker.showImagePicker({title: 'Select Image'}, response => {
+          if (response.uri) {
+            setUserImage(response.uri);
+          }
+        });
+    };
+
+    const handleChangePassword = () => {
+        //Perform actions for change password here
+    };
+
+}
