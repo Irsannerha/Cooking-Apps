94cef02a562a8776aeccb4b8a65266363f9b0ef5
Irsannerha <irsanrh275@gmail.com>
2023-05-15 17:50:34 +0700

    Upload Code Final

--- src/screen/DetailScreen.js
+++ src/screen/DetailScreen.js
@@ -25,3 +25,46 @@ const DetailScreen = () => {
   useEffect(() => {
     getRecipe();
   }, [getRecipe]);
+
+  return (
+    <View style={styles.container}>
+      <BackButton />
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../../img/bg.jpg')}
+          style={styles.gambarMakanan}
+        />
+      </View>
+      <View style={styles.deskripsiContainer}>
+        {isLoading && <ActivityIndicator size='large' color='#DF0606' />}
+        {!isLoading && (
+          <View>
+            <Text style={styles.namaMakanan}>{recipe.title}</Text>
+            <View style={styles.divider} />
+            <Text style={styles.title}>Bahan:</Text>
+            <View>
+              {recipe.ingredients &&
+                recipe.ingredients.map((ingredient, index) => (
+                  <Text key={index} style={styles.listBahan}>
+                    {ingredient}
+                  </Text>
+                ))}
+            </View>
+            <View style={styles.divider} />
+            <Text style={styles.title}>Cara membuat:</Text>
+            <View>
+              {recipe.steps &&
+                recipe.steps.map((step, index) => (
+                  <Text key={index} style={styles.caraMembuat}>
+                    {step}
+                  </Text>
+                ))}
+            </View>
+          </View>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default DetailScreen;
