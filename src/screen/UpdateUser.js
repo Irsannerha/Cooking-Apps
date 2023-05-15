40bf1fd1284a086481895918315685909de41f53
Irsannerha <irsanrh275@gmail.com>
2023-05-15 17:44:18 +0700

    Upload Code Final

--- src/screen/UpdateUser.js
+++ src/screen/UpdateUser.js
@@ -28,3 +28,52 @@ const UpdateUser = () => {
     const handleChangePassword = () => {
       // Perform actions for changing password here
     };
+
+    return (
+        <View style={styles.container}>
+          <BackButton />
+          {userImage && (
+            <TouchableOpacity onPress={handleImagePicker}>
+              <Image source={{ uri: userImage }} style={styles.userImage} />
+            </TouchableOpacity>
+          )}
+          {!userImage && (
+            <TouchableOpacity
+              style={styles.imagePickerButton}
+              onPress={handleImagePicker}
+            >
+              <Image
+                source={require('../../img/Uploads.jpg')}
+                style={styles.uploadIcon}
+              />
+              <Text style={styles.imagePickerButtonText}>Upload Foto</Text>
+            </TouchableOpacity>
+          )}
+          <View style={styles.centered}>
+            <TextInput
+              placeholder='Madazyan'
+              style={styles.textInput}
+              value={userName}
+              onChangeText={(text) => setUserName(text)}
+            />
+            <TextInput
+              placeholder='madazyan@mail.com'
+              style={styles.textInput}
+              value={userEmail}
+              onChangeText={(text) => setUserEmail(text)}
+            />
+            <TouchableOpacity
+              style={styles.changePasswordButton}
+              onPress={handleChangePassword}
+            >
+              <Text
+                style={styles.changePasswordButtonText}
+                onPress={() => navigation.navigate('UpdatePass')}
+              >
+                Ganti Password
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      );
+    };
