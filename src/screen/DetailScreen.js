0e45e5e9ab18aca5a25e6269caf1c2934cb449e0
Irsannerha <irsanrh275@gmail.com>
2023-05-15 17:49:45 +0700

    Upload Code Final

--- src/screen/DetailScreen.js
+++ src/screen/DetailScreen.js
@@ -3,3 +3,25 @@ import { View, Text, Image, StyleSheet, ActivityIndicator } from 'react-native';
 import BackButton from '../components/BackButton';
 import { useRoute } from '@react-navigation/native';
 import useHttp from '../hooks/use-http';
+
+const DetailScreen = () => {
+  const route = useRoute();
+  const { id } = route.params;
+  const [recipe, setRecipe] = useState([]);
+  const { isLoading, error, sendRequest } = useHttp();
+
+  const getRecipe = useCallback(async () => {
+    sendRequest(
+      {
+        url: `/api/v1/recipes/${id}`,
+      },
+      (responseData) => {
+        const data = responseData.data.data;
+        setRecipe(data);
+      }
+    );
+  }, [sendRequest, id]);
+
+  useEffect(() => {
+    getRecipe();
+  }, [getRecipe]);
