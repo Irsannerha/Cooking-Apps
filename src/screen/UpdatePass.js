18790db0dc4f0dad1b9f3cd025f93a3f56ee1574
anggrndewi <dewi.120140149@student.itera.ac.id>
2023-05-15 07:39:04 +0700

    insialisasi UpdatePass dan handleSavePassword

--- src/screen/UpdatePass.js
+++ src/screen/UpdatePass.js
@@ -1,2 +1,12 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const UpdatePass = () => {
+    const [oldPassword, setOldPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+  
+    const handleSavePassword = () => {
+      // Save new password to database or perform other actions here
+    };
+   
+};
