85092b4aa809711f4248baee9303b10f9443063c
Irsannerha <irsanrh275@gmail.com>
2023-05-15 17:39:05 +0700

    Upload Code Final

--- src/screen/UpdatePass.js
+++ src/screen/UpdatePass.js
@@ -14,3 +14,55 @@ const UpdatePass = () => {
   const [newPassword, setNewPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
   const { isLoading, sendRequest } = useHttp();
+
+  const handleSavePassword = () => {
+    sendRequest(
+      {
+        url: '/api/v1/users/updateMyPassword',
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: {
+          currentPassword: oldPassword,
+          newPassword: newPassword,
+          confirmPassword: passwordConfirm,
+        },
+      },
+      (data) => {
+        console.log(data);
+      }
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <BackButton />
+      <Text style={styles.title}>Ganti Password</Text>
+      <TextInput
+        secureTextEntry
+        placeholder='Masukkan Password Lama'
+        style={styles.textInput}
+        value={oldPassword}
+        onChangeText={(text) => setOldPassword(text)}
+      />
+      <TextInput
+        secureTextEntry
+        placeholder='Masukkan Password Baru'
+        style={styles.textInput}
+        value={newPassword}
+        onChangeText={(text) => setNewPassword(text)}
+      />
+      <TextInput
+        secureTextEntry
+        placeholder='Masukkan Password Baru confirm'
+        style={styles.textInput}
+        value={passwordConfirm}
+        onChangeText={(text) => setPasswordConfirm(text)}
+      />
+      <TouchableOpacity style={styles.saveButton} onPress={handleSavePassword}>
+        <Text style={styles.saveButtonText}>Simpan</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
