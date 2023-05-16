e822989e13b53b6defdc99cb9360b504aaa33c7a
Irsannerha <irsanrh275@gmail.com>
2023-05-16 10:44:34 +0700

    Upload Code

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -8,3 +8,40 @@ export default function BackButton(props) {
   const handlePress = () => {
     navigation.goBack();
   };
+
+  return (
+    <TouchableOpacity style={styles.backButton} onPress={handlePress}>
+      <Text style={styles.backButtonText}>Back</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  backButton: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    zIndex: 1,
+  },
+  backButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  cardResep: {
+    height: 150,
+    marginBottom: 15,
+    boxShadow: '0 0 10px rgba(0,0,0,0.5)',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    marginTop: 'auto',
+    marginBottom: 10,
+  },
+});
